453fe6cfffb525e7314198d7aa46c0a8f4f9a0a6
Mardu Swanepoel <merts.ms@gmail.com>
2025-12-03 12:55:29 +0100

    simplify base implementations

--- src/example_agent/agents/main.py
+++ src/example_agent/agents/main.py
@@ -6,9 +6,6 @@ from langchain.agents.middleware.types import AgentState
 from src.example_agent.tools.add_numbers import add_numbers
 from src.example_agent.tools.multiply_numbers import multiply_numbers
 from src.common.model_identifiers import ModelIdentifier
-from src.example_agent.agents.context import AgentContext
-from src.example_agent.agents.state import CustomAgentState
-from src.common.middleware import handle_tool_errors
 from src.example_agent.prompts.agent import SYSTEM_PROMPT
 
 _model = init_chat_model(model=ModelIdentifier.GPT_5, temperature=0.0, reasoning_effort="low")
@@ -19,8 +16,5 @@ _tools = [add_numbers, multiply_numbers]
 agent: CompiledStateGraph[AgentState[Any], Any, Any, Any] = create_agent(
     _model,
     _tools,
-    middleware=[handle_tool_errors],
-    context_schema=AgentContext,
-    state_schema=CustomAgentState,  # type: ignore[arg-type]
     system_prompt=SYSTEM_PROMPT,
 )
